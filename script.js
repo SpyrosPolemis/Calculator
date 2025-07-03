77809768140ca35014ad35c7ce5e3452e612cf64
SpyrosPolemis <spy.polemis@gmail.com>
2025-07-03 23:11:49 +1000

    Clicked number will appear in display

--- script.js
+++ script.js
@@ -13,3 +13,31 @@ function multiply (number1, number2) {
 function divide (number1, number2) {
     return number1 / number2;
 }
+
+const display = document.querySelector("#display")
+
+const numbers = document.querySelectorAll(".number")
+numbers.forEach((number) => {
+    number.addEventListener("click", () => {
+        display.innerHTML = (number.innerHTML)
+    })
+})
+
+// function operate (operator, number1, number2) {
+//     if (operator === "+") {
+//         add(number1, number2)
+//     } else if (operator === "+") {
+//         subtract(number1, number2)
+//     } else if (operator === "+") {
+//         multiply(number1, number2)
+//     } else if (operator === "+") {
+//         divide(number1, number2)
+//     } else {
+//         console.log("ERROR")
+//     }
+// press a button, add number to number1 
+// }
+
+// let number1;
+// let number2;
+// let operator;
