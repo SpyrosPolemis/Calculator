35a696f593e13eee7ff8e7f3f0500c5ddee642fa
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2025-07-10 20:22:01 +1000

    Refactor to handle equations with more than 2 numbers

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 function add (number1, number2) {
     let result = +number1 + +number2;
-    number1 = result
     display.innerHTML = result
+    return result;
 }
 
 function subtract (number1, number2) {
     let result = number1 - number2;
-    number1 = result
     display.innerHTML = result
+    return result
 }
 
 function multiply (number1, number2) {
     let result = number1 * number2;
-    number1 = result
     display.innerHTML = result
+    return result
 }
 
 function divide (number1, number2) {
     let result = number1 / number2;
-    number1 = result
     display.innerHTML = result
+    return result
 }
 
 const display = document.querySelector("#display")
@@ -39,9 +39,17 @@ numbers.forEach((number) => {
 const operators = document.querySelectorAll(".operator")
 operators.forEach((operator) => {
     operator.addEventListener("click", (event) => {
-        number1 = display.innerHTML
-        clearDisplay();
-        sign = event.target.innerHTML
+        console.log(sign, number1, number2)
+        if (sign && number1) {
+            number2 = display.innerHTML
+            calculate(sign, number1, number2)
+            number1 = display.innerHTML
+            sign = event.target.innerHTML
+        } else {
+            number1 = display.innerHTML
+            sign = event.target.innerHTML
+            clearDisplay();
+        }
     })
 })
 
@@ -54,17 +62,17 @@ equals.addEventListener("click", () => {
     calculate(sign, number1)
 })
 
-function calculate (operator, number1) {
-    number2 = display.innerHTML
-    console.log(operator)
+function calculate (operator, number1, number2) {
+    number2 = display.innerHTML;
+    console.log("calculating, this is number1:" + number1 + "number2:" + number2)
     if (operator === "+") {
-        add(number1, number2)
+        number1 = add(number1, number2)
     } else if (operator === "-") {
-        subtract(number1, number2)
+        number1 = subtract(number1, number2)
     } else if (operator === "x") {
-        multiply(number1, number2)
+        number1 = multiply(number1, number2)
     } else if (operator === "/") {
-        divide(number1, number2)
+        number1 = divide(number1, number2)
     } else {
         console.log("ERROR")
     }
